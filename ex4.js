4e9a52cc0ab5757f8aee7a0b7a2effc51a5fd26b
omiras <oscar.miras@gmail.com>
2023-10-16 23:40:37 +0200

    sol4

--- ex4.js
+++ ex4.js
@@ -4,15 +4,20 @@
 
 /**
  * Implementa el método "aprender"; que está parcialmente implementado. LA función recibe por parámetro un string, con el nuevo conocimiento aprendido. Debe actualizar la propiedad 'conocimientos' con dicho nuevo string.
+ *
+ * Resultado esperado: https://oscarm.tinytake.com/msc/ODgxMTQ3OV8yMjI0MDgyOQ
  */
 let estudiante = {
-    nombre: "Edu",
-    conocimientos: [],
-    aprender: function (nuevoConocimiento) {
-        // una línia de código
-    }
+  nombre: "Edu",
+  conocimientos: [],
+  aprender: function (nuevoConocimiento) {
+    // una línia de código
+    this.conocimientos.push(nuevoConocimiento);
+  },
+};
 
-}
-
-estudiante.aprender('HTML')
-console.log("Debería aparecer aquí que el estudiante ha aprendido HTML:", estudiante.conocimientos)
+estudiante.aprender("HTML");
+console.log(
+  "Debería aparecer aquí que el estudiante ha aprendido HTML:",
+  estudiante.conocimientos
+);
